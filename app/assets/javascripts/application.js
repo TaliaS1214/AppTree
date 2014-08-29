adfe6320a7d8060e80e805e710f4181ae0d1234c
TaliaS1214 <talias432@gmail.com>
2014-08-29 11:07:20 -0400

    Added Twilio stuff and initialized backbars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require underscore
+//= require backbone
+//= require handlebars
+//= require_tree ./backbone/routers
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
+//= require_tree ./templates
 //= require foundation
 //= require_tree .
 $(function() {
