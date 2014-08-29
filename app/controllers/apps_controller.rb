199793a82e286db487bf6ea2065618239f5f8f96
truotaku <najee.s.gardner@gmail.com>
2014-08-29 11:09:27 -0400

    removed turbo links, added itunes_id to apps and appstore model

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -12,4 +12,14 @@ class AppsController < ApplicationController
   def create
   end
 
+  def search
+    @results = AppStore.search(search_term)
+    render json: @results.to_json, status: 200
+  end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:name, :tags, :screenshot_urls, :release_date, :creator, :price, :small_avatar_url, :large_avatar_url, :track_view_url, :description, :user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829003310) do
+ActiveRecord::Schema.define(version: 20140829144025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140829003310) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "itunes_id"
   end
 
   create_table "genres", force: true do |t|
